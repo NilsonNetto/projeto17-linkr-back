7482cec02b739db1355a108cf7c72014372bec23
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-10-18 19:37:41 -0300

    Initial push

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
