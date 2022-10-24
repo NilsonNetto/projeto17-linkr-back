bea0a834eb4fb9398721977d4c7cf6241a3ce16a
Norival <norivaljunior.jf@gmail.com>
2022-10-24 11:12:24 -0300

    ajuste na rota de busca do usuario

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,10 @@
 import { findUser, showPageUser } from "../controllers/usersControllers.js";
 import express from "express";
-import { userLogged } from "../middleware/authMiddleware.js";
+import { validateToken } from "../middleware/validateTokenMiddleware.js";
 
 const router = express.Router();
 
-router.get("/user/:id", userLogged, showPageUser);
-router.get("/timeline/:name", userLogged, findUser);
+router.get("/user/:id", showPageUser);
+router.get("/users/:name", findUser);
 
 export default router;
