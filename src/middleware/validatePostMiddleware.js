74117bec016c10fd20193939cf9098bc931ca34f
Jean Campelo <jean93campelo@gmail.com>
2022-10-24 08:04:39 -0300

    resolving conflicts with postId

--- src/middleware/validatePostMiddleware.js
+++ src/middleware/validatePostMiddleware.js
@@ -2,11 +2,15 @@ import { validatePostRepository } from "../repositories/postRepository.js";
 import { serverErrorResponse } from "../controllers/controllerHelper.js";
 
 async function validadePost(req, res, next) {
-  const { postId } = req.body;
-  const { userId } = res.locals
+  let { postId } = req.body;
+  const { userId } = res.locals;
+
+  if (!postId) {
+    postId = req.params.postId;
+  }
 
   try {
-    const postRegistered = await validatePostRepository({postId, userId});
+    const postRegistered = await validatePostRepository({ postId, userId });
     if (postRegistered.rowCount === 0) {
       return res.status(401).send("Not authorized");
     }
