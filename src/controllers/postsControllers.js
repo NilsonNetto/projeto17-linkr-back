5ed4e3710c2275bd0796e0a1fd4374ecd11b8654
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-10-24 11:41:41 -0300

    feat: posts controller refactor to use repository pattern

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -2,13 +2,12 @@ import connection from "../db/db.js";
 import dotenv from "dotenv";
 import joi from "joi";
 import urlMetadata from "url-metadata";
+import { insertHashtag, insertPost, insertPostHashtag, listHashtag, listPosts } from "../repositories/postRepository.js";
 dotenv.config();
 
 async function publishPost(req, res) {
-  //COMO USUÁRIO LOGADO - MIDDLEWARE
   const userId = res.locals.userId;
 
-  //QUERO PUBLICAR UM POST
   const { description, link } = req.body;
 
   //VALIDAÇÃO - SE TEM LINK A SER PUBLICADO (SCHEMA)
@@ -33,29 +32,22 @@ async function publishPost(req, res) {
 
     if (hashtags.length > 0) {
       for (let i = 0; i < hashtags.length; i++) {
-        const hashtagExist = await connection.query('SELECT id FROM hashtags WHERE name = $1', [hashtags[i]]);
+        const hashtagExist = listHashtag(hashtags[i]);
         if (hashtagExist.rowCount === 0) {
-          await connection.query('INSERT INTO hashtags (name) VALUES ($1)', [hashtags[i]]);
+          insertHashtag(hashtags[i]);
         }
       }
     }
 
-    const post = await connection.query(
-      'INSERT INTO posts ("userId", description, url) VALUES ($1, $2, $3) RETURNING id',
-      [userId, description, link]
-    );
-    const postId = post.rows[0].id;
+    const postId = insertPost(userId, description, link);
 
-    const published = await connection.query(
-      'SELECT id AS "postId", description, url FROM posts WHERE id = $1',
-      [postId]
-    );
+    const published = listPublishedPost(postId);
 
     //INSERINDO NA TABELA POSTSHASHTAGS
     if (hashtags.length > 0) {
       for (let i = 0; i < hashtags.length; i++) {
-        const hashtagId = await connection.query('SELECT id FROM hashtags WHERE name = $1', [hashtags[i]]);
-        await connection.query('INSERT INTO "postsHashtags" ("postId", "hashtagId") VALUES ($1, $2)', [postId, hashtagId.rows[0].id]);
+        const hashtagId = listHashtag([hashtags[i]]);
+        insertPostHashtag(postId, hashtagId);
       }
     }
 
@@ -66,10 +58,8 @@ async function publishPost(req, res) {
 }
 
 async function getPosts(req, res) {
-  //COMO USUÁRIO LOGADO - MIDDLEWARE
   const userId = res.locals.userId;
 
-  //QUERO VER OS POSTS DA MINHA TIMELINE
   try {
     const posts = await listPosts(userId);
     const postsWithMetadatas = [];
@@ -93,34 +83,5 @@ async function getPosts(req, res) {
     res.status(500).send(error.message);
   }
 }
-
-const listPosts = async (userId) => {
-  return (
-    await connection.query(
-      `
-    SELECT
-      p.id ,
-      p."userId",  
-      u.username,
-      u."profilePicture",
-      p.description,
-      p.url,
-        CASE WHEN EXISTS (SELECT * FROM likes l2 WHERE l2."userId" = $1 AND l2."postId" = p.id)  
-        THEN TRUE
-        ELSE FALSE 
-      END AS "userLike" ,
-      array_agg(u2.username) AS "postLikes"
-    FROM posts p
-    JOIN users u ON p."userId" = u.id
-    LEFT JOIN likes l ON l."postId" = p.id
-    LEFT JOIN users u2 ON l."userId" = u2.id
-    GROUP BY p.id, u.username, u."profilePicture"
-    ORDER BY p.id DESC
-    LIMIT 20;
-    `,
-      [userId]
-    )
-  ).rows;
-};
 
 export { publishPost, getPosts };
